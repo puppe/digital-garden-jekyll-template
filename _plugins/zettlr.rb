219980c1e43ebf5b2425a0a407eb04db259d89b2
Martin Puppe <dev@mpuppe.de>
2022-02-18 22:20:28 +0100

    Improve handling of wiki links and backlinks
    Substituting the links and finding backlinks is now done with a more
    effienct approach. The algorithm‘s runtime is now linear in the number
    of notes instead of quadratic.
    
    Any occurrence of an ID like for example '20220218222506' (14 digits not
    enclosed in double brackets) in a note is now treated as an ID of that
    note. As before, the filename is also treated as an ID. All links using
    such an ID will link to the correct note. Links via the title of a note
    will not work anymore though.
    
    Backlinks are now also displayed for literature notes.
    
    Internal wiki links now look all the same: [◦]. This is because the text
    of a wiki link is not really meaningful in Zettlr anyway. This way, no
    space is wasted for 14-digit-long links, which should improve
    readability.

--- _plugins/zettlr.rb
+++ _plugins/zettlr.rb
@@ -1,6 +1,8 @@
+# coding: utf-8
 require 'json'
 require 'nokogiri'
 require 'open3'
+require 'set'
 
 module Jekyll
   module Converters
@@ -42,10 +44,13 @@ end
 
 module Jekyll
   module Zettlr
+    NOTE_ID_REGEX = '[[:alnum:]\-_+. ]+'
+    CITATION_ID_REGEX = '[[:alnum:]\-_+.]+'
+
     module BibInfoFilter
       def bib_info(id)
         site = @context.registers[:site]
-        reference = JSON.generate(site.data['bib'][id])
+        reference = JSON.generate(Jekyll::Zettlr::Generator.bib(site)[id])
 
         args = []
         args << "--style=#{site.config['zettlr']['csl']}"
@@ -70,9 +75,106 @@ module Jekyll
       end
     end
 
-    class LiteratureNoteGenerator < Jekyll::Generator
+    class Generator < Jekyll::Generator
       def initialize(config)
         @config = config['zettlr']
+      end
+
+      def generate(site)
+        bib = nil
+        open(site.config['zettlr']['bibliography']) do |f|
+          bib = load_bibliography(f)
+        end
+
+        notes = site.collections['notes'].docs
+        notes.concat(generate_literature_notes(site, bib))
+
+        id_map = build_id_map(notes)
+        graph = build_graph(notes, bib, id_map)
+
+        (site.pages + site.documents).each do |page|
+          substitute_wiki_links(page, id_map)
+        end
+
+        site.data['zettlr'] = {
+          'bib' => bib,
+          'id_map' => id_map,
+          'graph' => graph
+        }
+      end
+
+      private def substitute_wiki_links(page, id_map)
+        page.content.gsub!(/\[\[(#{NOTE_ID_REGEX})\]\]/) do |match|
+          id = $1
+          note = id_map[id]
+          if note
+            "<a class='internal-link note-link' href='#{note.url}'>[◦]</a>"
+          else
+            <<~HTML.delete("\n")
+              <span title='There is no note that matches this link.' class='invalid-link'>
+                <span class='invalid-link-brackets'>[[</span>
+                #{id}
+                <span class='invalid-link-brackets'>]]</span>
+              </span>
+            HTML
+          end
+        end
+      end
+
+      def self.bib(site)
+        site.data['zettlr']['bib']
+      end
+
+      def self.id_map(site)
+        site.data['zettlr']['id_map']
+      end
+
+      def self.graph(site)
+        site.data['zettlr']['graph']
+      end
+
+      class DirectedGraph
+        def initialize()
+          @out_edges = {}
+          @in_edges = {}
+        end
+
+        def add_edge(n1, n2) raise 'Loops are not allowed' if n1 == n2
+          raise "Nodes must not be nil" if n1.nil? || n2.nil?
+          @out_edges[n1] ||= Set.new
+          @in_edges[n2] ||= Set.new
+          @out_edges[n1] << n2
+          @in_edges[n2] << n1
+        end
+
+        def in_edges(node)
+          @in_edges[node]
+        end
+
+        def out_edges(node)
+          @out_edges[node]
+        end
+      end
+
+      private def build_graph(notes, bib, id_map)
+        graph = DirectedGraph.new
+
+        notes.each do |note|
+          note_links = note.content.scan(/(?<=\[\[)#{NOTE_ID_REGEX}(?=\]\])/)
+          citation_links = note.content.scan(/(?<![[:alnum:]])(?<=@)#{CITATION_ID_REGEX}/)
+          citation_links = citation_links.filter { |link| bib.has_key?(link) }
+          links = note_links + citation_links
+          links.each do |link|
+            other_note = id_map[link]
+            next if other_note.nil?
+            next if note == other_note
+            backlinks = other_note.data['backlinks'] ||= []
+            backlinks << note
+            # We don‘t use this graph yet, but we might write a JSON file
+            # in the future.
+            graph.add_edge(note, other_note)
+          end
+        end
       end
 
       private def populate_data(data, bib_entry)
@@ -90,36 +192,62 @@ module Jekyll
         data['bib_entry_json'] ||= JSON.generate(bib_entry)
       end
 
-      def generate(site)
-        bib = nil
-        open(@config['bibliography']) do |f|
-          bib = JSON.load(f)
-        end
-
+      private def generate_literature_notes(site, bib)
         literature_notes = {}
-        notes = site.collections['notes']
-        notes.docs.each do |note|
+        notes = site.collections['notes'].docs
+        notes.each do |note|
           break unless note.data['literature_note']
           id = note.data['slug']
           literature_notes[id] = note
         end
 
-        bib_hash = {}
-        bib.each do |entry|
-          id = entry['id']
-          bib_hash[id] = entry
+        new_notes = []
+        bib.each do |id, entry|
           note = literature_notes[id]
           if note.nil?
             note = Jekyll::Document.new(
               "#{site.source}/_notes/literatur/#{id}.md",
               {:site => site, :collection => site.collections['notes']})
-            notes.docs << note
+            new_notes << note
+            note.content = ''
             note.data['literature_note'] = true
             note.data['slug'] = entry['id']
           end
           populate_data(note.data, entry)
         end
-        site.data['bib'] = bib_hash
+
+        new_notes
+      end
+
+      private def load_bibliography(bibliography_file)
+        bib = nil
+        bib = JSON.load(bibliography_file)
+        bib_hash = {}
+        bib.each do |entry|
+          id = entry['id']
+          bib_hash[id] = entry
+        end
+        bib_hash
+      end
+
+      private def build_id_map(notes)
+        id_map = {}
+        add_to_map = -> (id, note) {
+          other_note = id_map[id]
+          if other_note
+            raise "The notes #{note.path} and #{other_note.path} share the same " +
+                  "identifier #{id}."
+          end
+          id_map[id] = note
+        }
+
+        notes.each do |note|
+          name = File.basename(note.basename, File.extname(note.basename))
+          add_to_map.call(name, note)
+          ids = note.content.scan(/(?<!\[\[)\d{14}/)
+          ids.each { |id| add_to_map.call(id, note) }
+        end
+        id_map
       end
     end
 
